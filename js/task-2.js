037d78fbe9cb835659941743f8314fa473a7606d
vladickjerseyclub <pakizhj@gmail.com>
2024-03-19 12:38:11 +0100

    first1

--- js/task-2.js
+++ js/task-2.js
@@ -6,7 +6,7 @@ function formatMessage(message, maxLength) {
     }
 }
 
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..." 
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
 console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
 console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
